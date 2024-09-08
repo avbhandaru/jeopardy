b12cd04b4adcd05b05122e2d2abc7b9a25a7d0e2
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2024-09-08 17:07:20 -0400

    SQL migrations and testing inserts, updates, and reads

--- backend/main.rs
+++ backend/main.rs
@@ -1,21 +1,26 @@
 extern crate lib;
 
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use diesel_async::{RunQueryDsl, AsyncConnection, AsyncPgConnection};
 
 // ordinary diesel model setup
 table! {
     users {
-        id -> Integer,
-        name -> Text,
+        id -> BigInt,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        username -> Text,
     }
 }
 
-#[derive(Queryable, Selectable, Debug)]
+#[derive(Queryable, Selectable, Insertable, Debug)]
 #[diesel(table_name = users)]
 struct User {
-    id: i32,
-    name: String,
+    id: i64,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+    username: String,
 }
 
 #[tokio::main]
@@ -24,18 +29,37 @@ async fn main() {
     let data = try_db().await;
     let hello = lib::Greeter::new("Hello");
     for user in data.iter() {
-        println!("User: {} {}", user.id, user.name);
-        hello.print(&user.name);
+        println!("User: {} {} {} {}", user.id, user.username, user.created_at, user.updated_at);
+        hello.print(&user.username);
     }
 }
 
 async fn try_db() -> Vec<User> {
     let mut connection = AsyncPgConnection::establish(&std::env::var("DATABASE_URL").unwrap()).await.unwrap();
 
+    let results = diesel::insert_into(users::table)
+        .values(&vec![
+            users::username.eq("Akhil"),
+            users::username.eq("Matt"),
+            users::username.eq("Hana"),
+            users::username.eq("Alex")
+        ])
+        .returning(User::as_returning())
+        .get_results(&mut connection)
+        .await.unwrap();
+
+    println!("New users created: {:?}", results);
+
+    diesel::update(users::table)
+        .filter(users::username.eq("Hana"))
+        .set(users::username.eq("LetterCook"))
+        .execute(&mut connection)
+        .await.unwrap();
+
     // use ordinary diesel query dsl to construct your query
     users::table
         .filter(users::id.gt(0))
-        .or_filter(users::name.like("%Luke"))
+        .or_filter(users::username.like("%Luke"))
         .select(User::as_select())
         // execute the query via the provided
         // async `diesel_async::RunQueryDsl`
