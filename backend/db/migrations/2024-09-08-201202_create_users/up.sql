93a92f462ef14f5c2929894d3913e2f4b9ad145d
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2024-09-08 17:09:53 -0400

    SQL migrations and testing inserts, updates, and reads

---
+++ backend/db/migrations/2024-09-08-201202_create_users/up.sql
@@ -0,0 +1,13 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    username TEXT NOT NULL
+);
+
+CREATE TRIGGER users_updated_at
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+EXECUTE PROCEDURE diesel_set_updated_at();
