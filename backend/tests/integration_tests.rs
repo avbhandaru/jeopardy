124528548155d04a70a781f88c5ac56f798b7062
mschwimmer <mschwimmer1234@gmail.com>
2024-10-15 18:39:49 -0400

    graphql getUser test

--- backend/tests/integration_tests.rs
+++ backend/tests/integration_tests.rs
@@ -1,6 +1,8 @@
 // tests/integration_tests.rs
 
 mod setup;
+use core::error;
+
 use async_graphql::Response;
 use async_graphql::{EmptyMutation, EmptySubscription, Request, Schema};
 use backend::db::pool::DBPool;
@@ -14,6 +16,7 @@ use chrono::Utc;
 use diesel::prelude::*; // Import Diesel prelude
 use diesel::ExpressionMethods;
 use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
+use http::request;
 // use serde_json::json;
 use setup::setup_test_db;
 
@@ -102,6 +105,62 @@ async fn test_create_user_graphql() -> Result<(), Box<dyn std::error::Error + Se
 
     // Check that the response does not contain errors
     assert!(response.errors.is_empty());
+
+    // Extract the "createUser" data from the response
+    let data = response.data.into_json().unwrap();
+    let created_user = data["createUser"].clone();
+
+    // Validate that the returned user matches what we expect
+    assert_eq!(created_user["username"], "testuser");
+    assert!(created_user["id"].as_i64().is_some()); // Ensure an ID was returned
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_get_user_graphql() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Set up test database and schema
+    let pool: DBPool = setup_test_db().await.unwrap();
+
+    // Only testing UserQuery, so no need for mutation or subscription
+    let schema: Schema<UserQuery, EmptyMutation, EmptySubscription> =
+        Schema::build(UserQuery, EmptyMutation, EmptySubscription)
+            .data(pool.clone())
+            .finish();
+
+    // Define Graphql query
+    let query = r#"
+        query {
+            getUser(id: 1) {
+                id
+                username
+                createdAt
+                updatedAt
+            }
+        }
+    "#;
+
+    // Execute the query and store response
+    let request: Request = Request::new(query);
+    let response: Response = Schema::execute(&schema, request).await;
+
+    // Print the errors to see what went wrong
+    if !response.errors.is_empty() {
+        println!("GraphQL errors: {:?}", response.errors);
+    } else {
+        println!("GraphQL data: {:?}", response.data);
+    }
+
+    // Check that the response does not contain errors
+    assert!(response.errors.is_empty());
+
+    // Extract the "createUser" data from the response
+    let data = response.data.into_json().unwrap();
+    let user = data["getUser"].clone();
+
+    // Validate that the returned user matches what we expect
+    assert_eq!(user["username"], "Akhil");
+    assert_eq!(user["id"], 1); // Ensure an ID was returned
 
     Ok(())
 }
