963a8895550030c71ab30760f664d869e16ca352
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2024-10-28 02:37:27 -0400

    Games page start

--- frontend/.next/static/chunks/src_app_layout_tsx_600847._.js
+++ frontend/.next/static/chunks/src_app_layout_tsx_600847._.js
@@ -5,8 +5,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_layout_tsx_600847._.js",
   "chunks": [
-    "static/chunks/src_app_c7d265._.css",
-    "static/chunks/src_app_layout_tsx_20a9fc._.js"
+    "static/chunks/[root of the server]__04172a._.css",
+    "static/chunks/node_modules_0dc44a._.js",
+    "static/chunks/src_app_9c9763._.js"
   ],
   "source": "dynamic"
 });
